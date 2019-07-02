29e0ed3d64d14359b41ce669f448024efb1e17ae
anyiv <anyeliv02@gmail.com>
2019-07-01 22:46:40 -0400

    apps organizadas más login feo

--- Artemis/settings.py
+++ Artemis/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'index',
+    'peticion',
+    'queja',
+    'reclamo',
+    'sugerencia',
+    'usuario',
+    'departamento',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +61,7 @@ ROOT_URLCONF = 'Artemis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ queja/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QuejaConfig(AppConfig):
+    name = 'queja'
