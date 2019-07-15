395f3178e8edd4354de134127d15107d44cddf0c
anyiv <anyeliv02@gmail.com>
2019-07-15 10:47:49 -0400

    u-u

--- apps/reclamo/urls.py
+++ apps/reclamo/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listadereclamos/', views.claimlist, name='claimlist'),
-    path('listadereclamosfinalizados/', views.claimfinished, name='claimfinished'),
-     path('reclamosproxavencer/', views.claimexpire, name='claimexpire'),
+    path('listado/', views.claimlist, name='claimlist'),
+    path('listadadofinalizados/', views.claimfinished, name='claimfinished'),
+    path('reclamosproxavencer/', views.claimexpire, name='claimexpire'),
+    path('registrarreclamo/', views.claimexpire, name='claimexpire'),
 ]
