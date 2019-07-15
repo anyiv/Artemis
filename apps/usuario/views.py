8e12d5d8cc8714c11b86f58237814d3f0f1296e7
Anyi Villarreal <anyeliv02@gmail.com>
2019-07-15 10:46:00 -0400

    tipos de usuario - parte 1/?

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def dashboard_checker(request):
-    #aqui va un codigo que aun no pienso
-    return render(request, 'usuario/gestreclamo/dashboard_gestorR.html', {})    
+    if request.user.type_u.codigo == 'cli':
+        return redirect('cliente/')
+    elif request.user.type_u.codigo == 'grec':
+        return redirect('gestorR/')
+    elif request.user.type_u.codigo == 'atc':
+        return redirect('atencioncli/')
+    elif request.user.type_u.codigo == 'gpqs':
+        return redirect('gestorPQS/')
+    elif request.user.type_u.codigo == 'tc':
+        return redirect('tecnico/')
+    elif request.user.type_u.codigo == 'admin':
+        return redirect('admin/')
+    elif request.user.type_u.codigo == 'ger':
+        return redirect('gerente')
 
 def dashboard_gestorR(request):
     return render(request, 'usuario/gestreclamo/dashboard_gestorR.html', {})
 
 def dashboard_cliente(request):
     return render(request, 'usuario/cliente/dashboard_cliente.html', {})
+
+def dashboard_atencioncli(request):
+    return render(request, 'usuario/atencioncli/dashboard_atencioncli.html', {})
+
+def dashboard_gestorPQS(request):
+    return render(request, 'usuario/gestpqs/dashboard_gestorPQS.html', {})
+
+def dashboard_tecnico(request):
+    return render(request, 'usuario/tecnico/dashboard_tecnico.html', {})
+
+def dashboard_admin(request):
+    return render(request, 'usuario/admin/dashboard_admin.html', {})
+
+def dashboard_gerente(request):
+    return render(request, 'usuario/gerente/dashboard_gerente.html', {})
