5531f0a969f8089ccd5abb34341c237e066ce6c1
anyiv <anyeliv02@gmail.com>
2019-07-15 11:21:59 -0400

    algo

--- apps/reclamo/urls.py
+++ apps/reclamo/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('listado/', views.claimlist, name='claimlist'),
     path('listadadofinalizados/', views.claimfinished, name='claimfinished'),
     path('reclamosproxavencer/', views.claimexpire, name='claimexpire'),
-    path('registrarreclamo/', views.claimexpire, name='claimexpire'),
+    path('registrarreclamo/', views.createclaim, name='createclaim'),
 ]
