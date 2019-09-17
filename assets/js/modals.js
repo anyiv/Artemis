03845dff928be7b172335f1d00449537002747e8
Anyi V <anyeliv02@gmail.com>
2019-09-16 21:26:12 -0400

    mejoras

---
+++ assets/js/modals.js
@@ -0,0 +1,5 @@
+$('.js-modal-buttons .btn').on('click', function () {
+    var color = $(this).data('color');
+    $('#mdModal .modal-content').removeAttr('class').addClass('modal-content modal-col-' + color);
+    $('#mdModal').modal('show');
+});
